a4f67e132187553b8418c5d245915e80c72c5886
Howard M. Harte <hharte@cyngn.com>
2015-05-22 09:54:20 -0700

    minnow_max: search for cm.mk in additional directories
    Intel's product configuration files are structured slightly
    differentily than others.  Device-specific configuration is nested
    one additional level, as follows:
    
    device/intel/<family>/<product>/
    
    Change-Id: I78b02978dc759b94024e5c5533d1108ac2634549
    
    Conflicts:
    	core/product_config.mk

--- core/product_config.mk
+++ core/product_config.mk
@@ -179,9 +179,10 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
+<<<<<<< HEAD
 # A PX build needs only the PX product makefiles.
 ifneq ($(PX_BUILD),)
-  all_product_configs := $(shell ls device/*/$(PX_BUILD)/px.mk)
+  all_product_configs := $(shell find device -path "*/$(PX_BUILD)/px.mk")
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
